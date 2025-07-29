a78dc1cfc5baf8169f8179d7e1e4b6b26d8d2a94
daanniill <daniil.malakhov12@gmail.com>
2025-07-28 19:36:52 -0700

    completed the round system

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
+const maxRounds = 5;
 
 function getComputerSelection() {
     let num_choice = Math.floor(Math.random() * 3);
@@ -50,7 +52,33 @@ function playRound(humanChoice, computerChoice) {
 
     resultText += `<br>Score: Human ${humanScore} - Computer ${computerScore}`;
     resultDiv.innerHTML = resultText;
+
+    roundsPlayed++;
+
+    if (roundsPlayed === maxRounds) {
+        showFinalResult();
+        disableButtons();
+    }
 }; 
+
+function showFinalResult() {
+    const finalResultDiv = document.querySelector("#final-result");
+    let finalMessage = `<br><strong>Final Score:</strong> Human ${humanScore} - Computer ${computerScore}<br>`;
+    if (humanScore > computerScore) {
+        finalMessage += "🎉 You win the game!";
+    } else if (humanScore < computerScore) {
+        finalMessage += "💻 Computer wins the game!";
+    } else {
+        finalMessage += "🤝 It's a tie!";
+    }
+    finalResultDiv.innerHTML = finalMessage;
+}
+
+function disableButtons() {
+    document.querySelector("#rock").disabled = true;
+    document.querySelector("#paper").disabled = true;
+    document.querySelector("#scissors").disabled = true;
+}
 
 function setupGame() {
     const choices = ["rock", "paper", "scissors"]
